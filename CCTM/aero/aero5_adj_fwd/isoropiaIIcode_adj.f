acaf359f0002427bc146e6ed675ac7d21761592c
scapps <scapps@gatech.edu>
2013-08-27 12:35:52 -0400

    Make fwd / bwd ISO code same; refine error checking

--- CCTM/aero/aero5_adj_fwd/isoropiaIIcode_adj.f
+++ CCTM/aero/aero5_adj_fwd/isoropiaIIcode_adj.f
@@ -9240,7 +9240,7 @@ C
          CALL FUNCG5A (PSI6LO, Y3)
          X3 = PSI6LO
          CALL PUSHERR (0002, 'CALCG5')    ! WARNING ERROR: NO CONVERGENCE
-         GOTO 50
+         RETURN                           ! no PUSH/POP yet in adjoint (8.2013.slc)
       ENDIF
 C
 C *** PERFORM BISECTION ***********************************************
@@ -9389,7 +9389,7 @@ C
          CALL FUNCG5A (PSI6LO, Y3)
          X3 = PSI6LO
          CALL PUSHERR (0002, 'CALCG5')    ! WARNING ERROR: NO CONVERGENCE
-         GOTO 50
+         RETURN                           ! no PUSH/POP yet in adjoint (8.2013.slc)
       ENDIF
 C
 C *** PERFORM BISECTION ***********************************************
@@ -9890,12 +9890,12 @@ C
 C
 C *** NO SUBDIVISION WITH SOLUTION; IF ABS(Y2)<EPS SOLUTION IS ASSUMED
 C
-      IF ((ABS(Y2) > EPS).AND.TST1.AND.(I > NDIV+1)) THEN
+      IF ((ABS(Y2) > EPS).AND.TST1.AND.(I > NDIV)) THEN
          CALL RSTGAMP
          CALL FUNCH6A (PSI6LO, Y3)
          X3 = PSI6LO
          CALL PUSHERR (0002, 'CALCH6')    ! WARNING ERROR: NO CONVERGENCE
-         GOTO 50
+         RETURN                           ! no PUSH/POP yet in adjoint (8.2013.slc)
       ENDIF
 C
 C *** PERFORM BISECTION ***********************************************
@@ -10038,12 +10038,12 @@ C
 C
 C *** NO SUBDIVISION WITH SOLUTION; IF ABS(Y2)<EPS SOLUTION IS ASSUMED
 C
-      IF ((ABS(Y2) > EPS).AND.TST1.AND.(I > NDIV+1)) THEN
+      IF ((ABS(Y2) > EPS).AND.TST1.AND.(I > NDIV)) THEN
          CALL RSTGAMP
          CALL FUNCH6A (PSI6LO, Y3)
          X3 = PSI6LO
          CALL PUSHERR (0002, 'CALCH6')    ! WARNING ERROR: NO CONVERGENCE
-         GOTO 50
+         RETURN                           ! no PUSH/POP yet in adjoint (8.2013.slc)
       ENDIF
 C
 C *** PERFORM BISECTION ***********************************************
