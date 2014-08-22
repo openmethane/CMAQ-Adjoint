011c792cd924edb5cffb4d21ef5ca994c981b31a
Shannon Capps <capps.shannon@epa.gov>
2014-08-22 14:29:42 -0400

    Eliminate duplicative subroutine

--- CCTM/vdiff/acm2_inline_adj_bwd/write3_distr.f
+++ CCTM/vdiff/acm2_inline_adj_bwd/write3_distr.f
@@ -1,70 +1,54 @@
 
 C RCS file, release, date & time of last delta, author, state, [and locker]
-C $Header: /Volumes/Data/CVS/CMAQ_CVSrepos/CCTM/src/vdiff/acm2_inline_adj_fwd/wrdaymsg.f,v 1.1 2011/02/22 01:10:45 ppercell Exp $
+C $Header: /Volumes/Data/CVS/CMAQ_CVSrepos/CCTM/src/vdiff/acm2_inline_adj_fwd/write3_distr.f,v 1.1 2011/02/22 01:10:45 ppercell Exp $
 
 C what(1) key, module and SID; SCCS file; date and time of last delta:
 C %W% %P% %G% %U%
 
 C::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-      SUBROUTINE WRDAYMSG( JDATE, MESG )
+      FUNCTION WRITE3_DISTR ( FNAME, VNAME, JDATE, JTIME, DIM1, DIM2, DATA )
+     &                        RESULT ( SUCCESS )
 
 C-----------------------------------------------------------------------
-C  Description:
-C    Writes a text message to stdout and log file about which day is
-C    being processed
-C  Preconditions:
-C  Subroutines and Functions Called:
-C  Revision History:
-C    Created 9/99 by M. Houyoux
-C-----------------------------------------------------------------------
-C Modified from:
+C Function: Use I/O-API's WRITE3 and avoid using PWRITE3 to write layer
+C           fractions to processor unique fake-gridded files
 
-C Project Title: Sparse Matrix Operator Kernel Emissions (SMOKE) Modeling System
-C File: @(#)$Id: wrdaymsg.f,v 1.1 2011/02/22 01:10:45 ppercell Exp $
-C COPYRIGHT (C) 2002, MCNC Environmental Modeling Center
-C All Rights Reserved
-C See file COPYRIGHT for conditions of use.
-C Environmental Modeling Center
-C MCNC
-C P.O. Box 12889
-C Research Triangle Park, NC  27709-2889
-C smoke@emc.mcnc.org
-C Pathname: $Source: /Volumes/Data/CVS/CMAQ_CVSrepos/CCTM/src/vdiff/acm2_inline_adj_fwd/wrdaymsg.f,v $
-C Last updated: $Date: 2011/02/22 01:10:45 $ 
+C Note: This function must live in a file that has a ".f" extension!
+
+C Revision History:
+C     5 Dec 2007 J.Young: initial implementation
 C-----------------------------------------------------------------------
 
       IMPLICIT NONE
 
-C Includes:
+      CHARACTER( * ), INTENT( IN ) :: FNAME
+      CHARACTER( * ), INTENT( IN ) :: VNAME
+      INTEGER,        INTENT( IN ) :: JDATE, JTIME
+      INTEGER,        INTENT( IN ) :: DIM1, DIM2
+      REAL,           INTENT( IN ) :: DATA( DIM1,DIM2 )
+      LOGICAL SUCCESS
 
-C External Functions:
-      CHARACTER( 14 ), EXTERNAL :: MMDDYY
-      INTEGER,         EXTERNAL :: WKDAY
+      LOGICAL, EXTERNAL :: WRITE3
+      INTEGER, EXTERNAL :: SETUP_LOGDEV
 
-C Arguments:
-      INTEGER       , INTENT(  IN ) :: JDATE    ! Julian date
-      CHARACTER( * ), INTENT( OUT ) :: MESG     ! message buffer
-
-C Parameters:
-      CHARACTER( 9 ), PARAMETER :: WKDAYS( 7 ) =
-     &                 ( / 'Monday   ', 'Tuesday  ', 'Wednesday', 'Thursday ',
-     &                     'Friday   ', 'Saturday ', 'Sunday   ' / )
-
-C Local variables:
-      INTEGER         DAY          !  day of week number
-      INTEGER         L            !  length of day name
-
-      CHARACTER( 16 ) :: PNAME = 'WRDAYMSG' ! procedure name
+      LOGICAL, SAVE :: FIRSTIME = .TRUE.
+      INTEGER LOGDEV
+      CHARACTER( 96 ) :: XMSG = ' '
 
 C-----------------------------------------------------------------------
 
-      DAY = WKDAY( JDATE )
+      SUCCESS = .TRUE.
 
-      L = LEN_TRIM( WKDAYS( DAY ) )
-      MESG = '>>> Processing ' // WKDAYS( DAY )( 1:L ) // ' ' // MMDDYY( JDATE )
-      CALL M3MSG2( MESG )
+      IF ( FIRSTIME ) THEN
+         FIRSTIME = .FALSE.
+         LOGDEV = SETUP_LOGDEV()
+      END If
+
+      IF ( .NOT. WRITE3( FNAME, VNAME, JDATE, JTIME, DATA ) ) THEN
+         XMSG = 'Error writing ' // 'to file "' // TRIM( FNAME ) // '."'
+         WRITE( LOGDEV,* ) XMSG
+         SUCCESS = .FALSE.
+      END IF
 
       RETURN
-
-      END SUBROUTINE WRDAYMSG
-
+      END FUNCTION WRITE3_DISTR
