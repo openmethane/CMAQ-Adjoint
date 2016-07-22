2c06d9386b3b9859323ac59062f09bb235aa95de
Steven Thomas <steven.thomas@unimelb.edu.au>
2016-07-22 15:58:57 +1000

    modified model to only use a single gas species - CO, test case before adding CO2 - added new mech file: cb05cl_ae5_aq_CO2only - bldit now reference to noop for chem and phot as well as aero - run script only looks for CO - added new test forcing file generating python script - modified model: some lines assumed a species with special requirements always existed, added conditional check e.g.: if ( HONO_HIT > 0 ) then ...

---
+++ scripts/force_adj_CO.py
@@ -0,0 +1,79 @@
+#! /apps/python/2.7.6/bin/python2.7
+
+from netCDF4 import Dataset
+import numpy as np
+
+import sys
+
+dates = [ str(20070600 + i) for i in range(10,14) ]
+
+for dstamp in dates:
+
+    #?using CONC or ACONC?
+    conc_name = '/home/563/spt563/cmaq_adj/CMAQadjBnmkData/GHG_output/CONC.' + dstamp
+    conc_file = Dataset(conc_name, mode='r', open=True)
+
+    COhr = np.squeeze(conc_file.variables['CO'][:][:][:])
+
+    dayCOhrly = COhr[0:25, 1, :, :]
+    binCOmax = np.where(dayCOhrly[:,:,:] == np.max(dayCOhrly[:,:,:], axis=0), 1.0, 0.0)
+    
+    forcCOmxhrpath = '/home/563/spt563/cmaq_adj/CMAQadjBnmkData/GHG_output/ADJ_FORCE.' + dstamp
+    forcCOmxhrfile = Dataset(forcCOmxhrpath, 'w', format='NETCDF3_64BIT')
+
+    attrs = ["IOAPI_VERSION", "EXEC_ID", "FTYPE",
+             "CDATE", "CTIME", "WDATE", "WTIME",
+             "SDATE", "STIME", "TSTEP", "NTHIK",
+             "NCOLS", "NROWS", "GDTYP", "P_ALP",
+             "P_BET", "P_GAM", "XCENT", "YCENT",
+             "XORIG", "YORIG", "XCELL", "YCELL",
+             "VGTYP", "VGTOP", "VGLVLS", "GDNAM", "HISTORY"]
+
+    for attr in attrs:
+        if hasattr(conc_file, attr):
+            attr_val = getattr(conc_file, attr)
+            setattr(forcCOmxhrfile, attr, attr_val)
+
+    setattr(forcCOmxhrfile, "NVARS", 1)
+    setattr(forcCOmxhrfile, "NLAYS", 1)
+    setattr(forcCOmxhrfile, "UPNAM", "RD_FORCE_FILE")
+    setattr(forcCOmxhrfile, "VAR-LIST", "CO             ")
+    setattr(forcCOmxhrfile, "FILEDESC", "Adjoint forcing file. Cost function: sum of max CO in every cell, each day (ppm)")
+
+    ltime, lrow, lcol = (binCOmax.shape)
+    llay = 1
+    ldattim = 2
+    ltime = 25
+
+    forcCOmxhrfile.createDimension("TSTEP", None)
+    forcCOmxhrfile.createDimension("DATE-TIME", ldattim)
+    forcCOmxhrfile.createDimension("LAY", llay)
+    forcCOmxhrfile.createDimension("VAR", 1)
+    forcCOmxhrfile.createDimension("ROW", lrow)
+    forcCOmxhrfile.createDimension("COL", lcol)
+
+    forcCOmxhrfile.sync()
+
+    dattim = np.squeeze(conc_file.variables['TFLAG'][:][:])
+    #dattim = dattim[0:ltime, 1, :]
+    dattime = dattim[0:ltime, :]
+
+    forc_tflag = forcCOmxhrfile.createVariable('TFLAG', 'i4', ('TSTEP', 'VAR', 'DATE-TIME'))
+    forc_tflag[:] = dattim.reshape(ltime, llay, ldattim)
+
+    forc_COmxhr = forcCOmxhrfile.createVariable('CO', 'f4', ('TSTEP', 'LAY', 'ROW', 'COL'))
+    forc_COmxhr[:] = binCOmax.reshape(ltime, llay, lrow, lcol)
+
+    varattrs = ["long_name", "units", "var_desc"]
+    for varattr in varattrs:
+        if hasattr(conc_file.variables['TFLAG'], varattr):
+            varattr_val = getattr(conc_file.variables['TFLAG'], varattr)
+            setattr(forc_tflag, varattr, varattr_val)
+        if hasattr(conc_file.variables['CO'], varattr):
+            varattr_val = getattr(conc_file.variables['CO'], varattr)
+            setattr(forc_COmxhr, varattr, varattr_val)
+        else:
+            print 'CO has no attr', varattr
+
+    forcCOmxhrfile.close()
+    conc_file.close()
